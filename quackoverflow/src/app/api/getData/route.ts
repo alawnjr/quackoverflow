44a74ff6c4a0d9edd883bf3e88c0771b637c5359
aliciagupta <alicia.gupta001@gmail.com>
2025-10-04 17:43:47 -0400

    create 11 endpoint

---
+++ quackoverflow/src/app/api/getData/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import analyzeCodeWithGemini from "./getGeminiFeedback";
+function getUserCode() {
+	return `print("Hello world")`
+}
+
+export async function GET() {
+	try {
+		const userCode = getUserCode();
+		const result = await analyzeCodeWithGemini(userCode);
+		
+		return NextResponse.json(result);
+	}
+
+	catch (err: any) {
+		return NextResponse.json({error: String(err)}, {status: 500});
+		//if there is an error, start catch block
+	}
+}
