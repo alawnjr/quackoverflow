c9bb308c0ea6794fc09d86f097e3dd889d9214ff
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2025-10-04 22:38:37 -0400

    redeploy

--- quackoverflow/src/app/api/getData/route.ts
+++ quackoverflow/src/app/api/getData/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import analyzeCodeWithGemini from "./getGeminiFeedback";
-import { useCodeStore } from "@/store/codeStore";
+import { useCodeStore } from "@/store/codeStore" ;
 
 export async function GET() {
   try {
